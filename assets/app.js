a0b8950f6989332bab051b8fa7172bc4cd84f353
Patrick O'Hara <pohara9720@cox.net>
2017-10-12 15:56:28 -0700

    asd

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+      $('.slider').slider();
+        $('select').material_select();
+
+      var $message = $('.full');
+$message.addClass('fadeIn fast');
+$(".home").addClass('fadeOut')
+
+setTimeout(function(){
+   $message.removeClass('fadeIn').addClass('fadeOut');
+   $(".home").removeClass("fadeOut").addClass("fadeIn fast");
+   $(".load").addClass("hide");
+}, 3000);
+
+  });
