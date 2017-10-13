fac3b14b64c82d11498d1f43ce6795a347ea0783
Patrick O'Hara <pohara9720@cox.net>
2017-10-12 20:07:03 -0700

    asd

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,16 @@
- $(document).ready(function(){
-      $('.slider').slider();
-        $('select').material_select();
+ $(document).ready(function() {
+     $('.slider').slider();
+     $('select').material_select();
+     $('.materialboxed').materialbox();
 
-      var $message = $('.full');
-$message.addClass('fadeIn fast');
-$(".home").addClass('fadeOut')
+     var $message = $('.full');
+     $message.addClass('fadeIn fast');
+     $(".home").addClass('fadeOut')
 
-setTimeout(function(){
-   $message.removeClass('fadeIn').addClass('fadeOut');
-   $(".home").removeClass("fadeOut").addClass("fadeIn fast");
-   $(".load").addClass("hide");
-}, 3000);
+     setTimeout(function() {
+         $message.removeClass('fadeIn').addClass('fadeOut');
+         $(".home").removeClass("fadeOut").addClass("fadeIn fast");
+         $(".load").addClass("hide");
+     }, 3000);
 
-  });
+ });
